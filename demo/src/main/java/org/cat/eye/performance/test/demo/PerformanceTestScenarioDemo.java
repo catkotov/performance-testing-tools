fb92624c58efda02df6b34c85483e30bf896acba
cat-k <cat-kotov@yandex.ru>
2022-04-24 18:18:39 +0300

    Java doc comments were added.

--- demo/src/main/java/org/cat/eye/performance/test/demo/PerformanceTestScenarioDemo.java
+++ demo/src/main/java/org/cat/eye/performance/test/demo/PerformanceTestScenarioDemo.java
@@ -1,17 +1,19 @@
 package org.cat.eye.performance.test.demo;
 
 import org.cat.eye.performance.test.PerformanceTestScenario;
-
 import java.util.function.Consumer;
 
+/**
+ * This a demo of performance test scenario library using.
+ */
 public class PerformanceTestScenarioDemo {
     public static void main(String[] args) {
-
-        Consumer<String> consumer = obj -> System.out.println("Send message: " + obj);
-        String obj = "to EB";
-
-        PerformanceTestScenario<String> scenario = new PerformanceTestScenario<>();
-        scenario
+        // object on that should be done an action
+        String obj = "to AWS EventsBridge";
+        // action that should be done on the object
+        Consumer<String> consumer = object -> System.out.println("Send message: " + object);
+        // create performance test scenario and play this scenario
+        new PerformanceTestScenario<String>()
                 .addEpisode(obj, consumer, 10, 10)
                 .addEpisode(obj, consumer, 50, 40)
                 .addEpisode(obj, consumer, 20, 20)
